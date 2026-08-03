bd3ea116d133e13eab2c4613a60e8fe9b115dfbd
raimei <gigae0108@gmail.com>
2026-08-03 10:05:52 +0900

    08/03/2026

---
+++ Programmers/12953.cpp
@@ -0,0 +1,14 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12953
+// 12953 N개의 최소공배수
+
+#include <string>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+int solution(vector<int> arr) {
+    int answer = arr[0];
+    for (int i = 1; i < arr.size(); i++) answer = lcm(answer, arr[i]);
+    return answer;
+}
