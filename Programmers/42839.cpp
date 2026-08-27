f45f7a7edad951da325ceeed5d03df32fa642639
raimei <gigae0108@gmail.com>
2026-08-27 09:45:38 +0900

    08/27/2026

---
+++ Programmers/42839.cpp
@@ -0,0 +1,35 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42839
+// 42839 소수 찾기
+
+#include <algorithm>
+#include <string>
+#include <unordered_set>
+
+using namespace std;
+
+bool is_prime(int n) {
+  if (n <= 1) return false;
+  if (n <= 3) return true;
+  if (n % 2 == 0 || n % 3 == 0) return false;
+  for (int i = 5; i * i <= n; i += 6) {
+    if (n % i == 0 || n % (i + 2) == 0) return false;
+  }
+  return true;
+}
+
+int solution(string numbers) {
+  unordered_set<int> primes;
+  unordered_set<int> checked;
+  sort(numbers.begin(), numbers.end());
+
+  do {
+    for (int i = 1; i <= numbers.size(); i++) {
+      int target = stoi(numbers.substr(0, i));
+      if (checked.contains(target)) continue;
+      if (is_prime(target)) primes.insert(target);
+      checked.insert(target);
+    }
+  } while (next_permutation(numbers.begin(), numbers.end()));
+
+  return primes.size();
+}
