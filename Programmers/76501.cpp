fd558746272c9e21e26f55e03fab8e0700dd54a7
우상준 <gigae0108@gmail.com>
2026-09-20 23:49:50 +0900

    09/20/2026

---
+++ Programmers/76501.cpp
@@ -0,0 +1,14 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/76501
+// 76501 음양 더하기
+
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+  int answer = 0;
+  for (int i = 0; i < absolutes.size(); i++) {
+    answer += signs[i] ? absolutes[i] : (-1) * absolutes[i];
+  }
+  return answer;
+}
